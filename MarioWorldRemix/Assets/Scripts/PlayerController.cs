348551ffc495a8c243326265ce22c5afcaf06237
thatss0metta <mario.galanreyes@gmail.com>
2023-02-06 23:18:08 -0500

    UpdateToPlayerMovement

--- MarioWorldRemix/Assets/Scripts/PlayerController.cs
+++ MarioWorldRemix/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float horizontal;
+    private float speed = 5f;
+    private float jumpingPower = 16f;
+    private bool isFacingRight = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,36 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal");
+        if(Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        if(isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
     }
 }
